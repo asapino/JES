207b292e675a1d4722eb9119d6b1e635b63d737d
asapino <andrewrienye09@gmail.com>
2023-12-12 16:44:34 +0300

    "00001"

--- Java/app.js
+++ Java/app.js
@@ -4,7 +4,6 @@ const path = require('path');
 
 const app = express();
 
-// Set up middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
--- Java/script.js
+++ Java/script.js
@@ -1,7 +1,77 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function () {
-    // Add your dynamic JavaScript features here
-    // For example, you can add form validation or other interactive elements.
-  });
   
+  const contactForm = document.getElementById('contactForm');
+  contactForm.addEventListener('submit', function (event) {
+    event.preventDefault(); 
+
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const messageInput = document.getElementById('message');
+
+    const nameValue = nameInput.value.trim();
+    const emailValue = emailInput.value.trim();
+    const messageValue = messageInput.value.trim();
+
+    if (nameValue === '' || emailValue === '' || messageValue === '') {
+      alert('Please fill in all fields');
+      return;
+    }
+
+    alert('Form submitted successfully!'); 
+    contactForm.reset(); 
+  });
+
+  const sliderImages = document.querySelectorAll('.slider-img');
+  let currentSlide = 0;
+
+  function showSlide(index) {
+    sliderImages.forEach((img, i) => {
+      img.style.display = i === index ? 'block' : 'none';
+    });
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % sliderImages.length;
+    showSlide(currentSlide);
+  }
+
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + sliderImages.length) % sliderImages.length;
+    showSlide(currentSlide);
+  }
+
+  showSlide(currentSlide);
+
+  const nextButton = document.getElementById('nextButton');
+  const prevButton = document.getElementById('prevButton');
+
+  nextButton.addEventListener('click', nextSlide);
+  prevButton.addEventListener('click', prevSlide);
+
+  const tooltips = document.querySelectorAll('.tooltip');
+  tooltips.forEach((tooltip) => {
+    tooltip.addEventListener('mouseover', function () {
+      const tooltipText = this.getAttribute('data-tooltip');
+      const tooltipElement = document.createElement('div');
+      tooltipElement.className = 'custom-tooltip';
+      tooltipElement.textContent = tooltipText;
+
+      document.body.appendChild(tooltipElement);
+
+      const rect = this.getBoundingClientRect();
+      tooltipElement.style.top = rect.top - tooltipElement.offsetHeight + 'px';
+      tooltipElement.style.left = rect.left + rect.width / 2 - tooltipElement.offsetWidth / 2 + 'px';
+    });
+
+    tooltip.addEventListener('mouseout', function () {
+      const tooltipElement = document.querySelector('.custom-tooltip');
+      if (tooltipElement) {
+        tooltipElement.remove();
+      }
+    });
+  });
+
+  const nav = document.querySelector('.nav');
+
+
+});
